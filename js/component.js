5440887a1f12c8fc88b44174960d683587cbfb51
Pradyumna Shome <PradyGameDev+git@gmail.com>
2018-02-24 12:54:07 -0600

    Removes velocity and position. Access position directly from the mesh. Acceleration is used to update velocity.

--- js/component.js
+++ js/component.js
@@ -1,29 +1,23 @@
 class Component {
 
     constructor(position, mesh) {
-        this.position_ = position;
         this.mesh_ = mesh;
-        this.mesh_.position.x = this.position_.x;
-        this.mesh_.position.y = this.position_.y;
-        this.mesh_.position.z = this.position_.z;
+        this.mesh_.position.x = position.x;
+        this.mesh_.position.y = position.y;
+        this.mesh_.position.z = position.z;
 
         this.geometry_ = new THREE.BoxGeometry(this.position_);
         this.color_ = 0x00ff00;
         this.material_ = new THREE.MeshBasicMaterial({color: this.color_});
 
         this.bounciness_ = 0.0;
-        this.velocity_ = new THREE.Vector3(0, 0, 0);
         this.acceleration_ = new THREE.Vector3(0, 0, 0);
 
         this.pointValue_ = 0;
     }
 
     update() {
-        // Keep mesh in sync with position
-        // cleaner way to do this? are they both vetor3 objects?
-        this.mesh_.position.x = this.position_.x;
-        this.mesh_.position.y = this.position_.y;
-        this.mesh_.position.z = this.position_.z;
+
     }
 
     physicsStep() {
@@ -74,14 +68,6 @@ class Component {
 
     set bounciness(bounciness) {
         this.bounciness_ = bounciness;
-    }
-
-    get velocity() {
-        return this.velocity_;
-    }
-
-    set velocity(velocity) {
-        this.velocity_ = velocity;
     }
 
     get acceleration() {
