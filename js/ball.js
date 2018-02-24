dd8a0c03e75f0f7f533aaf661a949f9d546dfee9
Pradyumna Shome <PradyGameDev+git@gmail.com>
2018-02-24 13:06:44 -0600

    Adds collision listener to ball, which should increase its point value.

--- js/ball.js
+++ js/ball.js
@@ -2,19 +2,23 @@ class Ball extends Component {
     constructor(position, mesh) {
         super(position, mesh);
         mesh.castShadow = true;
+        mesh.addEventListener( 'collision', onCollision(other, relative_velocity, relative_rotation, contact_normal));
     }
     launch() {
-        let accel = 3;
-        this.acceleration_ = new Vector3(0, 0, accel);
-        this.velocity_ += this.acceleration_;
+        this.acceleration_ = new THREE.Vector3(0, 0, 2);
+        this.mesh_.setLinearVelocity(this.getLinearVelocity() + this.acceleration_);
     }
     update() {
         // Apply gravity
-        if (this.acceleration_ > new Vector3(0, 0, 0))
-        {
-            this.acceleration_ -= new Vector3(0, 0, .5);
-        }
-        this.velocity_ -= new Vector3(0, -1, 0);
+        // Reduce acceleration
+        let gravity = new THREE.Vector3(0, -1, 0);
+        this.mesh_.setLinearVelocity(this.mesh_.getLinearVelocity() + gravity);
         // Check for collision
+    }
+    onCollision(other, relative_velocity, relative_rotation, contact_normal)
+    {
+         // `this` has collided with `other_object` with an impact speed of `relative_velocity` and a rotational force of `relative_rotation` and at normal `contact_normal`
+         this.setLinearVelocity(relative_velocity * (1.0/ (this.bounciness * other.bounciness)));
+         this.pointValue_ += other.pointValue_;
     }
 }
