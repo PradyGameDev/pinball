569f38c87e4a118d090ac7cdada53175716f283f
Pradyumna Shome <PradyGameDev+git@gmail.com>
2018-02-23 21:50:31 -0600

    Refactors variable names

--- js/component.js
+++ js/component.js
@@ -2,82 +2,82 @@ class Component
 {
     constructor() {
         //Position
-        this.position = new THREE.Vector3(0, 0, 0);
+        this.position_ = new THREE.Vector3(0, 0, 0);
         //Built in mesh object has geometry and material
         //Mesh has geometry + material
-        this.geometry = new THREE.BoxGeometry(position); 
-        this.color = 0x00ff00;
-        this.material = new THREE.MeshBasicMaterial({color}); 
-        this.mesh = new THREE.Mesh(geometry, material);
+        this.geometry_ = new THREE.BoxGeometry(position); 
+        this.color_ = 0x00ff00;
+        this.material_ = new THREE.MeshBasicMaterial({color}); 
+        this.mesh_ = new THREE.Mesh(geometry, material);
         //Floating point value representing extent of bounciness
-        this.bounciness = 0.0;
-        this.velocity = new THREE.Vector3(0, 0, 0);
-        this.acceleration = new THREE.Vector3(0, 0, 0);
+        this.bounciness_ = 0.0;
+        this.velocity_ = new THREE.Vector3(0, 0, 0);
+        this.acceleration_ = new THREE.Vector3(0, 0, 0);
         //Represents the point value of each component. The ball has no pointValue, whereas each of the collidable game objects have a score
         //that gets added to each player's score upon collision.
-        this.pointValue = 0;
+        this.pointValue_ = 0;
     }
 
-    //Initializer
+    //Quick initializer
     setup(position, mesh, bounciness, velocity, acceleration, pointValue) {
-        this.position = position;
-        this.mesh = mesh; //THREE.js mesh
-        this.bounciness = bounciness;
-        this.velocity = velocity;
-        this.acceleration = acceleration;
-        this.pointValue = pointValue;
+        this.position_ = position;
+        this.mesh_ = mesh; //THREE.js mesh
+        this.bounciness_ = bounciness;
+        this.velocity_ = velocity;
+        this.acceleration_ = acceleration;
+        this.pointValue_ = pointValue;
     }
 
     get position() {
-        return this.position;
+        return this.position_;
     }
 
     get mesh() {
-        return this.mesh;
+        return this.mesh_;
     }
 
     get bounciness() {
-        return this.position;
+        return this.position_;
     }
 
     get velocity() {
-        return this.velocity;
+        return this.velocity_;
     }
 
     get acceleration() {
-        return this.acceleration;
+        return this.acceleration_;
     }
 
     get pointValue() {
-        return this.pointValue;
+        return this.pointValue_;
     }
 
     set position(position) {
-        this.position = position;
+        this.position_ = position;
     }
 
     set mesh(mesh) {
-        this.mesh = mesh;
+        this.mesh_ = mesh;
     }
 
     set bounciness(bounciness) {
-        this.bounciness = bounciness;
+        this.bounciness_ = bounciness;
     }
 
     set velocity(velocity) {
-        this.velocity = velocity;
+        this.velocity_ = velocity;
     }
 
     set acceleration(acceleration) {
-        this.acceleration = acceleration;
+        this.acceleration_ = acceleration;
     }
 
     set pointValue(pointValue) {
-        this.pointValue = pointValue;
+        this.pointValue_ = pointValue;
     }
 
     raycast(raycaster, intersects) {
-        return this.mesh.raycast(raycaster, intersects);
+        return this.mesh_.raycast(raycaster, intersects);
     }
 
     collidedWith(other) {
