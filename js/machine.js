e279f626a6bca830dfaad8d01f12f84b888f4cf2
Reed Oei <oei.reed@gmail.com>
2018-02-23 22:45:56 -0600

    Made flippers move.

--- js/machine.js
+++ js/machine.js
@@ -1,14 +1,20 @@
-function Machine() {
-  this.components = [];
+class Machine {
+    constructor() {
+        this.components = [];
 
-  // Floor
-  var width = 50;
-  var height = 50;
-  var geometry = new THREE.PlaneGeometry(width, height, 4, 4);
-  var material = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+        // Floor
+        var width = 50;
+        var height = 50;
+        this.geometry = new THREE.PlaneGeometry(width, height, 4, 4);
+        this.material = new THREE.MeshLambertMaterial({ color: 0x0000ff });
 
-  var floor = new THREE.Mesh(geometry, material);
-  floor.receiveShadow = true;
-  floor.rotateX(-Math.PI / 2);
-  scene.add(floor);
+        var floor = new THREE.Mesh(this.geometry, this.material);
+        floor.receiveShadow = true;
+        floor.rotateX(-Math.PI / 2);
+        scene.add(floor);
+    }
+
+    update() {
+
+    }
 }
